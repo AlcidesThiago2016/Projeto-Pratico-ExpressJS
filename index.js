ee8bdd9b0f34761188709ddc65942d024315fdcd
Alcides Thiago <alcides_thiago@hotmail.com>
2024-07-22 19:15:59 +0000

    create route ´/welcome´

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express")
 const app = express()
 
 app.get("/", function (req, res) {
-    res.send("Hello World")
+    res.send("Hello Express!")
+})
+
+app.get("/welcome", function (req, res) {
+    res.send("Seja Bem-Vindo a missão ExpressJS")
 })
 
 app.listen(3000)
